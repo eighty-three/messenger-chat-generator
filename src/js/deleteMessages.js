caa1f51fd3fe7c446285913176a260c7a0404f07
Eighty Three <cwp1@protonmail.com>
2020-05-18 05:24:40 +0800

    Add incomplete deleteBubble function

--- src/js/deleteMessages.js
+++ src/js/deleteMessages.js
@@ -16,3 +16,25 @@ export const deleteItem = () => {
   }
 };
 
+export const deleteBubble = () => { //Basic structure, class/styling not yet included
+  if (messagesContainer.firstElementChild) {
+    let latestMessage = messagesContainer.lastElementChild; 
+    if (!(latestMessage.classList.contains('js-timestamp'))) {
+      let extraBubblesContainer = document.querySelector('class^=js-extra-bubbles-container');
+      let lastBubbleContainer = document.querySelector('class^=js-last-bubble-container');
+      if (!extraBubblesContainer.firstChild) {
+        deleteItem();
+      } else if (extraBubblesContainer.children === 1) {
+        lastBubbleContainer.lastElementChild.remove();
+        extraBubblesContainer.firstElementChild.className = (latestMessage.classList.contains('js-message--other') && extraBubblesContainer.firstElementChild.tagName !== 'IMG')
+          ? 'classname'
+          : 'classname';
+        lastBubbleContainer.prepend(extraBubblesContainer.firstElementChild);
+      } else {
+        lastBubbleContainer.lastElementChild.remove();
+        lastBubbleContainer.prepend(extraBubblesContainer.lastElementChild);
+      }
+    }
+  }
+};
+
