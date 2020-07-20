b25969ab905befecf3df06d3faa08de3e5d0bb5b
Eighty Three <cwp1@protonmail.com>
2020-07-20 10:04:53 +0800

    Fix to accomodate path to static assets

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, 'build')));
+const projectPath = (process.env.NODE_ENV !== 'production')
+  ? '/'
+  : '/projects/messenger-chat-generator'
 
-app.get('/', (req, res) => {
+app.use('/static', express.static(path.join(__dirname, 'build')));
+
+app.get(projectPath, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = 3001;
+const PORT = 3002;
 server.listen(PORT, () => {
   console.log(`Server on port ${server.address().port}`);
 });
